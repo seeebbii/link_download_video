b055c468cb3bc3a5c426632e5cbac15c6bb1bb5a
haseeb zafar <Haseebzafar107@gmail.com>
2021-01-22 16:44:44 +0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/codeminers/link_download_video/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.codeminers.link_download_video
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,91 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
+import 'package:path_provider/path_provider.dart';
+
+void main() => runApp(MaterialApp(
+  home: MyApp(),
+  debugShowCheckedModeBanner: false,
+));
+
+class MyApp extends StatefulWidget {
+  @override
+  MyAppState createState() {
+    return new MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  final imgUrl = "https://media.istockphoto.com/photos/pakistan-monument-islamabad-picture-id535695503?k=6&m=535695503&s=612x612&w=0&h=uP8aDK4xlfjk3kEiyr9wwUiuh80UwAiICweFpiBDosk=";
+  bool downloading = false;
+  var progressString = "";
+
+  @override
+  void initState() {
+    super.initState();
+
+    downloadFile();
+  }
+
+  Future<void> downloadFile() async {
+    Dio dio = Dio();
+
+    try {
+      var dir = await getApplicationDocumentsDirectory();
+
+      await dio.download(imgUrl, "${dir.path}/myimage.jpg",
+          onReceiveProgress: (rec, total) {
+            print("Rec: $rec , Total: $total");
+
+            setState(() {
+              downloading = true;
+              progressString = ((rec / total) * 100).toStringAsFixed(0) + "%";
+            });
+          });
+    } catch (e) {
+      print(e);
+    }
+
+    setState(() {
+      downloading = false;
+      progressString = "Completed";
+    });
+    print("Download completed");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("AppBar"),
+      ),
+      body: Center(
+        child: downloading
+            ? Container(
+          height: 120.0,
+          width: 200.0,
+          child: Card(
+            color: Colors.black,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircularProgressIndicator(),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Text(
+                  "Downloading File: $progressString",
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                )
+              ],
+            ),
+          ),
+        )
+            : Text("No Data"),
+      ),
+    );
+  }
+}
