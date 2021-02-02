de92f9895bc4c7c1bba65edcb7d6684be7c8bbcf
haseeb zafar <Haseebzafar107@gmail.com>
2021-02-02 15:00:25 +0500

    latest update

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -686,7 +686,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 ),
               ),
             ),
-            SizedBox(height: 100,)
+            SizedBox(height: 90,)
           ],
         ),
       ),
